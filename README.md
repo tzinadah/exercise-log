9f9070ded90c7e09a980fb29202257d537e8a8f1
Talal Abuzinadah <primezinadah@gmail.com>
2024-10-20 17:55:31 +0100

    Add server basic functionality

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 This is an api designed to keep track of users and their exercise log
 
 API endpoints are:
-GET /register -> return registery page
 GET / -> return main api page
+GET /register -> return registery page
 GET /users -> return json file of all usernames
 GET /$username/log -> return page for adding exercise
 GET /$username -> return exercies of a user
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Init
+dotenv.config();
+const server = express();
+const __dirname = import.meta.dirname;
+const PORT = process.env.PORT || 3000;
+
+// Connecting to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("Connected to database"))
+  .catch((err) => console.error(err));
+
+// Logger middleware
+server.use((req, res, next) => {
+  console.log("request: ", req.method, req.url);
+  next();
+});
+
+// Middleware for post body parsing
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// Static get endpoints
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+server.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/user-form.html");
+});
+
+server.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/exercise-form.html");
+});
+
+// Starting server
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
